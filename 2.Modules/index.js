0e86b866a3a990bc9f23e1133daa5c3a1d7eebdf
pavansta <pavan.learn69@gmail>
2024-10-15 17:41:11 +0530

    now i have exported all person from peoples to index displying working perfectly

--- 2.Modules/index.js
+++ 2.Modules/index.js
@@ -1,7 +1,11 @@
 const greet = require("./greet");
-const person2 = require("./peoples");
-console.log(person1);
-console.log(person2);
+// const person2 = require("./peoples");
+// console.log(person1);
+const { person1, person2, person3 } = require("./peoples");
+console.log(
+  `first name:${person1} second name ${person2} and last name ${person3}`
+);
+
 greet.greet1("NodeJs");
 const sum = greet.add(5, 3);
 const difference = greet.subtract(5, 3);
--- 2.Modules/peoples.js
+++ 2.Modules/peoples.js
@@ -3,5 +3,7 @@ let person2 = "Kalyan";
 let person3 = "Sharma";
 
 // if want to export single person
-module.exports =  person2
+//module.exports = person2;
 
+// if we want to export all persons or modules use {}
+module.exports = { person1, person2, person3 };
