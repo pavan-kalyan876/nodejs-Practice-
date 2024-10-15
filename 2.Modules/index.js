a9dfe3ac54c00f6cf503f3b6ccad100e5f0698a5
pavansta <pavan.learn69@gmail>
2024-10-15 17:35:40 +0530

    created seperate file in that created three person, how to export single module, done perfectly diplaying in index js

--- 2.Modules/index.js
+++ 2.Modules/index.js
@@ -1,5 +1,7 @@
 const greet = require("./greet");
-
+const person2 = require("./peoples");
+console.log(person1);
+console.log(person2);
 greet.greet1("NodeJs");
 const sum = greet.add(5, 3);
 const difference = greet.subtract(5, 3);
---
+++ 2.Modules/peoples.js
@@ -0,0 +1,7 @@
+let person1 = "Pavan";
+let person2 = "Kalyan";
+let person3 = "Sharma";
+
+// if want to export single person
+module.exports =  person2
+
