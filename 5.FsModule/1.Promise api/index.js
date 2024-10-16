d57eacddf23e331ac686ece272dbd94a9f1d38e5
pavansta <pavan.learn69@gmail>
2024-10-16 15:52:20 +0530

    added recursive beacuse it checks folder is created or not

--- 5.FsModule/1.Promise api/index.js
+++ 5.FsModule/1.Promise api/index.js
@@ -3,7 +3,7 @@ import * as fs from "fs/promises";
 //creating a directory
 
 try {
-  await fs.mkdir("C:\\node js\\courses"); // it wil create folder in my laptop
+  await fs.mkdir("C:\\node js\\courses\\pavan", { recursive: true }); // it wil create folder in my laptop
   console.log("Folder Created ...");
 } catch (error) {
   console.log(error);
