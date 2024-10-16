4ea4405b5a5380bd03d55e88d2dd63e6e05be53f
pavansta <pavan.learn69@gmail>
2024-10-16 10:49:54 +0530

    writte end js it will discarded in output

--- 4.Path Module/index.js
+++ 4.Path Module/index.js
@@ -2,3 +2,5 @@ import path from "path";
 
 //basename is a method provided by the path module, which is used to retrieve the last part of a file path
 console.log(path.basename("c:\\nodejs\\filename.js")); //filename
+console.log(path.basename("c:\\nodejs\\app.html.js")); //app.html.js
+console.log(path.basename("c:\\nodejs\\app.html.js", ".js")); 
