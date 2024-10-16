9c16ef6884dc1b9a1298f653d85ca775abc0950e
pavansta <pavan.learn69@gmail>
2024-10-16 11:16:55 +0530

    join path has been done

--- 4.Path Module/index.js
+++ 4.Path Module/index.js
@@ -14,3 +14,11 @@ console.log(dirname); // Output: /users/pavan/documents
 const ext = "/users/pavan/documents/file.js";
 const extnam = path.extname(ext);
 console.log(extnam); // output will be .js
+
+//path: module that joins multiple path segments into a single path.
+const joinPath = path.join("/users", "pavan", "documents", "file.txt");
+console.log(joinPath); //\users\pavan\documents\file.txt
+
+//path.join is commonly used when you need to construct paths dynamically in a cross-platform way
+const joinpat = path.join("/users", "pavan", "..", "documents", "file.txt");
+console.log(joinpat); //Output: /users/documents/file.txt
