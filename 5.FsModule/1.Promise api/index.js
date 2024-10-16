9d725e763e299a8cc3bfd15e3e01c484e93d5a6d
pavansta <pavan.learn69@gmail>
2024-10-16 16:00:34 +0530

    created how to read file from folder or dir

--- 5.FsModule/1.Promise api/index.js
+++ 5.FsModule/1.Promise api/index.js
@@ -1,10 +1,21 @@
 import * as fs from "fs/promises";
 
 //creating a directory
+// try {
+//   await fs.mkdir("C:\\node js\\courses\\pavan", { recursive: true }); // it wil create folder in my laptop
+//   console.log("Folder Created ...");
+// } catch (error) {
+//   console.log(error);
+// }
+
+// reading a directory or content
 
 try {
-  await fs.mkdir("C:\\node js\\courses\\pavan", { recursive: true }); // it wil create folder in my laptop
-  console.log("Folder Created ...");
+  const files = await fs.readdir("c:\\sampleNode");
+
+  for (const file of files) {
+    console.log(file);
+  }
 } catch (error) {
-  console.log(error);
+  console.log("Error:", error);
 }
