05a5a2eeb37658af269d27b2b3475e628318e694
pavansta <pavan.learn69@gmail>
2024-10-16 14:26:35 +0530

    learned normalize

--- 4.Path Module/realTimeex.js
+++ 4.Path Module/realTimeex.js
@@ -1,7 +1,7 @@
 import path from "path";
 //const path = require("path");
 const filePath = "/users/pavan/uploads/images/profile.png";
-
+const filepath1 = "/users//pavan/documents///file.txt";
 // task one get the file name
 const fileName = path.basename(filePath);
 console.log("File Name:", fileName); // File Name: profile.png
@@ -12,3 +12,5 @@ console.log("directory Name: ", directoryName); // directory Name:  /users/pavan
 const fileExtension = path.extname(fileName);
 console.log("file Extension:", fileExtension); //file Extension: .png
 
+const unnormalized = path.normalize(filepath1);
+console.log("normalized:", unnormalized); //  \users\pavan\documents\file.txt
