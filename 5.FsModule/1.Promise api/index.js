51a39aa9a06c21e15ad5420ca151ffaa4dd87990
pavansta <pavan.learn69@gmail>
2024-10-16 15:42:58 +0530

    created fsModule folder, under 3 folder created, in promise created folder in my laptop successfulr

---
+++ 5.FsModule/1.Promise api/index.js
@@ -0,0 +1,10 @@
+import * as fs from "fs/promises";
+
+//creating a directory
+
+try {
+  await fs.mkdir("C:\\node js"); // it wil create folder in my laptop
+  console.log("Folder Created ...");
+} catch (error) {
+  console.log(error);
+}
