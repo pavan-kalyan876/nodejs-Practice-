2ff76f2cd2ace8808204d031d70aaadf851c8275
pavansta <pavan.learn69@gmail>
2024-10-16 10:46:33 +0530

    start with path basename is working proprly, basename return last part of file

---
+++ 4.Path Module/index.js
@@ -0,0 +1,4 @@
+import path from "path";
+
+//basename is a method provided by the path module, which is used to retrieve the last part of a file path
+console.log(path.basename("c:\\nodejs\\filename.js")); //filename
