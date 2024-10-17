8c0f130ed2ea405116b57ad4592e7fa162040897
pavansta <pavan.learn69@gmail>
2024-10-17 07:23:02 +0530

    done with os module, moniter system resorce like cpu,mem,uptime

---
+++ 6.OsModule/index.js
@@ -0,0 +1,11 @@
+import os from "os";
+console.log(os.platform()); // return the system platform like win32
+console.log(os.arch()); // return the cpu architecture x64
+console.log(os.type()); // return the os name('windows-nt'or'linux)
+console.log(os.release()); // return the os version
+console.log(os.uptime()); // return the uptime
+console.log(os.hostname()); // return the device name of setting
+console.log(os.totalmem()); // return memeory in bytes
+console.log(os.freemem()); // return free memory in bytes
+console.log(os.cpus()); // return the of cpu,model,sys,idle in arr obj
+console.log(os.networkInterfaces()); // return the network interface details
